56e6361337c39ba88a1f3912c1e6fc3ddd4f45d5
RUTVI98 <rutvidevani98@gmail.com>
2025-09-10 13:07:15 +0530

    Quiz section and result section of exam blade

--- resources/views/web/exam/index.blade.php
+++ resources/views/web/exam/index.blade.php
@@ -26,101 +26,102 @@
             <div class="row justify-content-center">
                 <div class="col-12 col-lg-6">
                     <div class="quiz-content-info">
-                        
 
-                    {{-- question card --}}
-                    @foreach ($questions as $index => $q )
-                        <div class="card quiz-card mb-3" data-correct="{{ $q->answer }}">
 
-                            <div class="card-body quiz-card-body">
-                                <form class="question-answer p-0" id="question-answer">
-                                    <div class="easy-to-use d-flex gap-2 align-items-center">
+                        {{-- question card --}}
+                        @foreach ($questions as $index => $q)
 
-                                        {{-- Timer --}}
-                                             <span class="quiz-time fs-14px timer" id="timer-{{ $index }}">30s</span> 
-                                                <p class="card-text mb-0 theme-color-161616 fs-18px">
-                                                    {{ $loop->iteration }}. {{ $q->question }} </p>
-                                                <br>
+                            <div class="card quiz-card mb-3" data-correct="{{ $q->answer }}">
 
-                                                @if($q->image)
+                                <div class="card-body quiz-card-body">
+                                    <form class="question-answer p-0" id="question-answer">
+                                        <div class="easy-to-use d-flex gap-2 align-items-center">
+
+                                            {{-- Timer --}}
+                                            <span class="quiz-time fs-14px timer" id="timer-{{ $index }}">30s</span>
+
+                                            <p class="card-text mb-0 theme-color-161616 fs-18px">
+                                                {{ $loop->iteration }}. {{ $q->question }}
+                                            </p>
+
+                                            @if($q->image)
                                                 <div class="my-3 text-center">
-                                                <img src="{{ asset('assets/image/ts-upload/ts-' . $q->image) }}" 
-                                                        alt="Question Image" class="img-fluid rounded" 
-                                                         style="max-width: 100px; max-height: 200px; object-fit: contain;">
-                                                    </div>
-                                                 @endif
-                                    </div>
-                                    
-                                    {{-- Options --}}
-                                    <label class="click-answer">
-                                        <input type="radio" name="q{{ $index }}" value="A" class="d-none">
-                                        <div class="select-answer d-flex align-items-center p-3">
-                                            <div class="click-with fs-14px">A</div>
-                                            <div class="option-texta">{{ $q->option_a }}</div>
+                                                    <img src="{{ asset('assets/image/ts-upload/ts-' . $q->image) }}"
+                                                        alt="Question Image" class="img-fluid rounded"
+                                                        style="max-width: 100px; max-height: 200px; object-fit: contain;">
+                                                </div>
+                                            @endif
                                         </div>
-                                    </label>
 
-                                    <label class="click-answer">
-                                        <input type="radio" name="q{{ $index }}" value="A" class="d-none">
-                                        <div class="select-answer d-flex align-items-center p-3">
-                                            <div class="click-with fs-14px">B</div>
-                                            <div class="option-texta">{{ $q->option_b }}</div>
+                                        {{-- Options --}}
+                                        <label class="click-answer">
+                                            <input type="radio" name="q{{ $index }}" value="{{ $q->option_a }}" class="d-none">
+                                            <div class="select-answer d-flex align-items-center p-3">
+                                                <div class="click-with fs-14px">A</div>
+                                                <div class="option-texta">{{ $q->option_a }}</div>
+                                            </div>
+                                        </label>
+
+                                        <label class="click-answer">
+                                            <input type="radio" name="q{{ $index }}" value="{{ $q->option_b }}" class="d-none">
+                                            <div class="select-answer d-flex align-items-center p-3">
+                                                <div class="click-with fs-14px">B</div>
+                                                <div class="option-texta">{{ $q->option_b }}</div>
+                                            </div>
+                                        </label>
+
+                                        <label class="click-answer">
+                                            <input type="radio" name="q{{ $index }}" value="{{ $q->option_c }}" class="d-none">
+                                            <div class="select-answer d-flex align-items-center p-3">
+                                                <div class="click-with fs-14px">C</div>
+                                                <div class="option-texta">{{ $q->option_c }}</div>
+                                            </div>
+                                        </label>
+
+
+                                        <div class="errorExam my-2" style="display:none"> You Have Not Select The Answer
                                         </div>
-                                    </label>
-                                    
-                                    <label class="click-answer">
-                                        <input type="radio" name="q{{ $index }}" value="A" class="d-none">
-                                        <div class="select-answer d-flex align-items-center p-3">
-                                            <div class="click-with fs-14px">C</div>
-                                            <div class="option-texta">{{ $q->option_c }}</div>
+
+                                        <div class="question-number d-flex align-items-center justify-content-between">
+                                            <p class="display-number mb-0 theme-color-161616 fw-600">
+                                                Question {{ $loop->iteration }}/{{ $questions->count() }}</p>
+
+                                            <div class="true-false-btn gap-2 d-flex">
+                                                <a class="true-btn d-flex justify-content-between" data-bs-toggle="modal"
+                                                    href="#">
+                                                    <span class="true-answer"><i class="fa-solid fa-check"></i></span>
+                                                    <span class="true-answer-counter">0</span>
+                                                </a>
+
+                                                <a class="false-btn d-flex justify-content-between" data-bs-toggle="modal"
+                                                    href="#">
+                                                    <span class="true-answer"><i class="fa-solid fa-xmark"></i></span>
+                                                    <span class="wrong-answer-counter">0</span>
+                                                </a>
+                                            </div>
                                         </div>
-                                    </label>
-
-                                    
-                                    {{-- <div class="errorExam my-2"> You Have Not Select The Answer
-                                    </div> --}}
-
-                                    <div class="question-number d-flex align-items-center justify-content-between">
-                                        <p class="display-number mb-0 theme-color-161616 fw-600">
-                                           Question {{ $loop->iteration }}/{{ $questions->count() }}</p>
-
-                                        <div class="true-false-btn gap-2 d-flex">
-
-                                            <a class="true-btn d-flex justify-content-between" data-bs-toggle="modal"
-                                                href="#">
-                                                <span class="true-answer"><i class="fa-solid fa-check"></i></span>
-                                                <span class="true-answer-counter" id="correct_answer_count">0</span>   
-                                            </a>
-
-                                            <a class="false-btn d-flex justify-content-between" data-bs-toggle="modal"
-                                                href="#">
-                                                <span class="true-answer"><i class="fa-solid fa-xmark"></i></span>
-                                                <span class="true-answer-counter" id="wrong_answer_count">0</span>
-                                            </a>
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                        <div class="card quiz-card card-position"></div>
-                        @endforeach
-            
-                         {{-- Next button --}}
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="next-button text-center mt-4 pt-2">
-                                 <a type="submit" form='question-answer' class="next" id="next">Next
-                                    <i class="fa-solid fa-chevron-right"></i></a>
+                                    </form>
                                 </div>
                             </div>
+                            <div class="card quiz-card card-position"></div>
+                        @endforeach
+                    </div>
+
+                    {{-- Next button --}}
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="next-button text-center mt-4 pt-2">
+                                <a type="submit" form='question-answer' class="next" id="next">Next
+                                    <i class="fa-solid fa-chevron-right"></i></a>
+                            </div>
                         </div>
-  
                     </div>
                 </div>
             </div>
         </div>
     </section>
     <!-- quiz section end -->
+
 
     <!-- result section start -->
     <section class="result-section">
@@ -135,10 +136,10 @@
                                         <p class="card-text" id="result-text"> Exam is Successfully Completed. </p>
                                         <h2 class="card-text" id="passed-text">Passed</h2>
                                         <h2 class="card-text" id="failed-text">Failed </h2>
-                                        <p class="card-text" id="correct_answer">Correct Answers:14 </p>
-                                        <p class="card-text" id="wrong_answer">Wrong Answers:06 </p>
-                                        <div
-                                            class="d-flex align-items-center justify-content-center gap-3 flex-column flex-ms-row">
+                                        <p class="card-text" id="correct_answer">Correct Answers: </p>
+                                        <p class="card-text" id="wrong_answer">Wrong Answers: </p>
+                                    
+                                        <div class="d-flex align-items-center justify-content-center gap-3 flex-column flex-ms-row">
                                             <a class="r-submit w-100 text-center fw-300" href="#">Home</a>
                                             <a class="r-submit w-100 text-center fw-300" href="#">ScoreBoard</a>
                                         </div>
@@ -156,11 +157,10 @@
 
                         <div
                             class="d-flex align-items-center justify-content-center flex-column flex-ms-row next-button gap-3">
-                            <a href="#" class="next  text-center">Next</a>
                             <a href="#" class="next text-center">
                                 <p class="mb-0">Retake Exam </p> <i class="fa-solid fa-chevron-right"></i>
                             </a>
-                        </div>  
+                        </div>
                     </div>
                 </div>
             </div>
@@ -205,7 +205,6 @@
                         <div class="card quiz-card card-position"></div>
                     </div>
                 </div>
-
             </div>
             <div class="row justify-content-center">
                 <div class="col-12 col-lg-6 mt-4">
@@ -222,9 +221,107 @@
 @endsection
 
 @push('scripts')
-<script>
+    <script>
+        $(document).ready(function () {
 
+            let currentIndex = 0;
+            let totalQuestions = $(".quiz-card[data-correct]").length;
+            let timer;
+            let timeleft = 30;
+            let correctCount = 0;
+            let wrongCount = 0;
 
+            $(".quiz-card[data-correct]").hide();
+            $(".quiz-card[data-correct]").eq(currentIndex).show();
 
-</script>
+            function updateTimer() {
+                $("#timer-" + currentIndex).text(timeleft + "s");
+            }
+
+            function startTimer() {
+                timeleft = 2;
+                updateTimer();
+                timer = setInterval(function () {
+                    timeleft--;
+                    updateTimer();
+                    if (timeleft < 0) {
+                        clearInterval(timer);
+                        checkAnswer(true);
+                    }
+                }, 1000);
+            }
+
+            function checkAnswer(timeout = false) {
+                let currentCard = $(".quiz-card[data-correct]").eq(currentIndex);
+                let correctAnswer = currentCard.data("correct");
+                let selected = currentCard.find("input[type=radio]:checked").val();
+
+                // If nothing selected and user clicked Next
+                if (!selected && !timeout) {
+                    currentCard.find(".errorExam").show();
+                    return false;
+                }
+
+                currentCard.find(".errorExam").hide();
+
+                // evaluate answer
+                if (selected == correctAnswer) {
+                    correctCount++;
+                    $(".true-answer-counter").text(correctCount);
+                } else {
+                    wrongCount++;
+                    $(".wrong-answer-counter").text(wrongCount);
+                }
+
+                moveNext();
+            }
+
+            function moveNext() {
+                clearInterval(timer);
+                $(".quiz-card[data-correct]").eq(currentIndex).hide();
+                currentIndex++;
+
+                if (currentIndex < totalQuestions) {
+                    $(".quiz-card[data-correct]").eq(currentIndex).show();
+                    startTimer();
+                } else {
+                    // Exam finished
+                    $(".quiz-section").hide();
+                    $(".result-section").show(); // show your result section
+                    $("#correct_answer").text("Correct Answers: " + correctCount);
+                    $("#wrong_answer").text("Wrong Answers: " + wrongCount);
+
+                    if (correctCount >= (totalQuestions / 2)) {
+                        $("#passed-text").show();
+                        $("#failed-text").hide();
+                    } else {
+                        $("#passed-text").hide();
+                        $("#failed-text").show();
+                    }
+                }
+            }
+
+            // when user selects an option -> clear error
+            $("input[type=radio]").on("change", function () {
+                $(".errorExam").hide();
+            });
+
+            // Next button
+            $("#next").on("click", function (e) {
+                e.preventDefault();
+                checkAnswer();
+            });
+
+            // hide result section initially
+            $(".result-section").hide();
+
+            startTimer();
+
+            // $("#langselect").on("change", function () {
+            //     language = $(this).val();
+
+            // });
+
+        });
+    </script>
 @endpush
